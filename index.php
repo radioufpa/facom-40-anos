9d8a80242fe75910d0b90168f8c1aa9cf11af94a
Lucas Mauess <lucasmauess@outlook.com>
2017-02-13 15:59:28 -0200

    v1

---
+++ index.php
@@ -0,0 +1,441 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+
+	<?php 
+
+	$page_name = '40 Anos Facom UFPA';
+	$page_description = 'A Rádio Web UFPA preparou uma programação especial em homenagem aos 40 anos da Faculdade de Comunicação da UFPA (Facom). Com professores, especialistas e profissionais no ramo da comunicação na região Norte.';
+	$page_tags = 'facom ufpa 40 anos, aniversario facom ufpa, programas facom ufpa, radio web ufpa facom';
+	$color = '#CC4D38';
+	$image_icon = 'http://radio.ufpa.br/facom-40-anos/_images/icon.png';
+	$image_share = 'http://radio.ufpa.br/facom-40-anos/_images/share.png';
+
+	include '../_includes/head.php';
+
+	?>
+
+	<link href="facom-40.css" rel="stylesheet" type="text/css">
+
+	<script src="../_scripts/newwindow.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+
+	<?php 
+	include '../_includes/header.php'; 
+	include '../_includes/share.php'; 
+	?>
+
+    <section class="full-screen color-2">
+
+    	<div class="full-screen-container img-logo">
+
+	        <h1 class="transparent">40 Anos Facom UFPA</h1>
+
+	        <a href="#apresentacao" id="scroll-button" class="color-1"></a>
+
+		</div>
+
+    </section>
+
+
+	<section id="apresentacao" class="full-screen img-1">
+
+        <div class="full-screen-container invert featured-text color-5">
+	    	<h2>Facom 40 anos: ganhando o mundo, voando juntos</h2>
+
+	    	<p>A Rádio Web UFPA preparou uma programação especial em homenagem aos 40 anos da Faculdade de Comunicação (Facom) da UFPA. A programação especial entrou no ar desde 01/02/2016 e está disponível aqui na integra.</p>
+
+			<p>Os programas especiais contam com a participação de professores, especialistas e profissionais no ramo da comunicação na região Norte e que também já passaram pela Faculdade de Comunicação no decorrer destes 40 anos, para falar sobre as lutas, as conquistas e a trajetória dos cursos de Jornalismo e Publicidade e Propaganda.</p>
+
+			<p>Sinta-se livre para navegar!</p>
+        </div>
+
+	</section>
+
+
+	<section id="ufpa-entrevista" class="full-screen">
+
+	<div class="full-screen-container invert color-3">
+
+		<div class="program-icon icon-ufpa-entrevista color-1"></div>
+		<h2 class="program-title">UFPA Entrevista</h2>
+
+		<p>O programa entrevistou personagens importantes na história do curso de comunicação social, com atuação nas áreas de Jornalismo e Publicidade e Propaganda. Professores, ex-alunos e profissionais falaram de sua trajetória na comunicação e contaram suas memórias com a Facom.</p>
+
+		<div class="card">
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/lucio-flavio-pinto/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/lucio-flavio-pinto/lucio-flavio-pinto.jpg');"></div>
+				<h3>Lúcio Flávio Pinto</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/milton-camargo/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/milton-camargo/milton-camargo.jpg');"></div>
+				<h3>Milton Camargo</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/manuel-dutra/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/manuel-dutra/manuel-dutra.jpg');"></div>
+				<h3>Manuel Dutra</h3>
+			</a>
+
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/luciana-miranda/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/luciana-miranda/luciana-miranda.jpg');"></div>
+				<h3>Luciana Miranda</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/rosaly-brito/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/rosaly-brito/rosaly-brito.jpg');"></div>
+				<h3>Rosaly Brito</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/luiz-cezar-santos/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/luiz-cezar-santos/luiz-cezar-santos.jpg');"></div>
+				<h3>Luiz Cézar Santos (LZ)</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/regina-alves/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/regina-alves/regina-alves.jpg');"></div>
+				<h3>Regina Alves</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/kleyton-silva/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/kleyton-silva/kleyton-silva.jpg');"></div>
+				<h3>Kleyton Silva</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/regina-lima/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/regina-lima/regina-lima.jpg');"></div>
+				<h3>Regina Lima</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/ismael-machado/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-entrevista/2016/ismael-machado/ismael-machado.jpg');"></div>
+				<h3>Ismael Machado</h3>
+			</a>
+
+		</div>
+
+	</div>
+
+	</section>
+
+
+	<section id="ufpa-ensino" class="full-screen">
+
+		<div class="full-screen-container invert color-4">
+
+			<div class="program-icon icon-ufpa-ensino color-1"></div>
+			<h2 class="program-title">UFPA Ensino</h2>
+			
+			<p>A Faculdade de Comunicação tem um trabalho importante dentro e fora da Universidade. Projetos independentes – idealizados principalmente por alunos do curso de comunicação – nascem dentro da Universidade e ganham vida além dos portões UFPA.</p>
+
+			<p>O UFPA Ensino abordou esses projetos que conquistaram público dentro e fora da UFPA. Na plataforma de Blog, conheça mais sobre “Os Apocalíticos”, “Belém Mundi” e “Nós Vamos Assim”. Na plataforma audiovisual, conheça os projetos “Repórter E” e o “Coletivo Palafita”. Na área do jornalismo cultural, conheça a revista “Na Cuia” e o site “Reduto Cult”, e, ainda, a “Muvuca na Cumbuca”, que é a semana de Comunicação da UFPA.</p>
+
+			<div class="card">
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-blogs/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-blogs/projetos-independentes-blogs.jpg');"></div>
+					<h3>Projetos Independentes - Blogs</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-audiovisual/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-audiovisual/projetos-independentes-audiovisual.jpg');"></div>
+					<h3>Projetos Independentes - Audiovisual</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-muvuca-jornalismo-cultural" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-ensino/2016/projetos-independentes-muvuca-jornalismo-cultural/projetos-independentes-muvuca-jornalismo-cultural.jpg');"></div>
+					<h3>Projetos Independentes - Muvuca e Jornalismo Cultural</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-ensino/2016/40-anos-facom-ufpa/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-ensino/2016/40-anos-facom-ufpa/40-anos-facom-ufpa.jpg');"></div>
+					<h3>40 anos da Facom UFPA</h3>
+				</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+
+	<section id="ufpa-comunidade" class="full-screen">
+
+		<div class="full-screen-container invert color-3">
+
+			<div class="program-icon icon-ufpa-comunidade color-1"></div>
+			<h2 class="program-title">UFPA Comunidade</h2>
+
+			<p>O UFPA Comunidade tratou de projetos de extensão que atuam na área de divulgação científica e produção de conteúdo audiovisual e impresso. Os convidados falam da importância para a comunicação local, servindo para o exercício do jornalismo e estimulando a divulgação das ações de pesquisa, extensão e de ensino realizadas na universidade. Os projetos também são laboratórios de experimentação para diferentes linguagens e um espaço de formação de novos comunicadores.</p>
+
+			<div class="card">
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/jornal-primeiras-linhas/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/jornal-primeiras-linhas/jornal-primeiras-linhas.jpg');"></div>
+					<h3>Jornal Primeiras Linhas</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/academia-amazonia/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/academia-amazonia/academia-amazonia.jpg');"></div>
+					<h3>Academia Amazônia</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/oficina-de-criacao/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/oficina-de-criacao/oficina-de-criacao.jpg');"></div>
+					<h3>Oficina de Criação</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/ocupa-belem/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/ocupa-belem/ocupa-belem.jpg');"></div>
+					<h3>Ocupa Belém</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/memoria-facom/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-comunidade/2016/memoria-facom/memoria-facom.jpg');"></div>
+					<h3>Memória Facom</h3>
+				</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+
+	<section id="ufpa-pesquisa" class="full-screen">
+
+		<div class="full-screen-container invert color-4">
+
+			<div class="program-icon icon-ufpa-pesquisa color-1"></div>
+			<h2 class="program-title">UFPA Pesquisa</h2>
+			
+			<p>O UFPA Pesquisa especial 40 anos da Facom UFPA, entrevistou professores importantes que desenvolvem pesquisas em diferentes áreas da Comunicação dentro da Universidade. As discussões abordam os grupos de pesquisas criados e a relação entre pesquisa acadêmica com o mercado de trabalho.</p>
+
+			<div class="card">
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/ppgcom-ufpa/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/ppgcom-ufpa/ppgcom-ufpa.jpg');"></div>
+					<h3>PPGCOM</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/pesquisas-publicidade/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/pesquisas-publicidade/pesquisas-publicidade.jpg');"></div>
+					<h3>Pesquisas em Publicidade</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/nuances-pesquisa-comunicacao/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/nuances-pesquisa-comunicacao/nuances-pesquisa-comunicacao.jpg');"></div>
+					<h3>Nuances da Pesquisa em Comunicação</h3>
+				</a>
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/pesquisas-jornalismo/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-pesquisa/2016/pesquisas-jornalismo/pesquisas-jornalismo.jpg');"></div>
+					<h3>Pesquisas em Jornalismo</h3>
+				</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section id="espaco-experimental" class="full-screen">
+
+		<div class="full-screen-container invert color-3">
+
+			<div class="program-icon icon-ufpa-debate color-1"></div>
+			<h2 class="program-title">UFPA Debate</h2>
+		
+			<p>O programa colocou em pauta a importância da discussão sobre comunicação nos contextos social e cidadão.</p>
+
+			<div class="card">
+
+				<a href="http://radio.ufpa.br/play/programas/ufpa-debate/2016/movimento-estudantil-comunicacao/" class="newWindow">
+					<div style="background-image: url('http://radio.ufpa.br/play/programas/ufpa-debate/2016/movimento-estudantil-comunicacao/caco.jpg');"></div>
+					<h3>Movimento Estudantil Comunicação</h3>
+				</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+
+	<section id="radiojornal-acontece" class="full-screen">
+
+		<div class="full-screen-container invert color-4">
+
+			<div class="program-icon icon-radiojornal-acontece color-1"></div>
+			<h2 class="program-title">Radiojornal Acontece</h2>
+				
+			<p>Em uma edição especial transmitida no dia 26 de fevereiro, o Radiojornal Acontece mostrou a cobertura do Seminário “A Comunicação em Movimento”, evento que abriu as comemorações pelos 40 anos do curso. As reportagens destacam as palestras, discussões e homenagens realizadas.</p>
+
+			<div class="card">
+
+			<a href="http://radio.ufpa.br/play/programas/radiojornal-acontece/2016/facom-ufpa-40-anos/" class="newWindow">
+				<div style="background-image: url('http://radio.ufpa.br/play/programas/radiojornal-acontece/2016/facom-ufpa-40-anos/facom-ufpa-40-anos.jpg');"></div>
+				<h3>Facom 40 Anos</h3>
+			</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+
+	<section id="depoimentos" class="full-screen">
+
+        <div class="full-screen-container invert color-2">
+
+        	<h2>Depoimentos</h2>
+
+        	<p>Alunos, professores e profissionais da comunicação no Pará, além representantes da administração superior, celebram os 40 anos do curso de comunicação social da UFPA.</p>
+
+        	<div class="track">
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__ALESSON_LUIS.mp3" class="newWindow">
+					<p>Alesson Lameira</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__ANA_CAROLINA.mp3" class="newWindow">
+					<p>Ana Carolina</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__BRENDA_RACHIT.mp3" class="newWindow">
+					<p>Brenda Rachit</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__CARLOS_MANESCHY.mp3" class="newWindow">
+					<p>Carlos Maneschy</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__CASSIA_NASCIMENTO.mp3" class="newWindow">
+					<p>Cássia Nascimento</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__DIOGO_MIRANDA.mp3" class="newWindow">
+					<p>Diogo Miranda</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__EDIR_PROENCA.mp3" class="newWindow">
+					<p>Edyr Proença</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__ELIDA_CRISTO.mp3" class="newWindow">
+					<p>Élida Cristo Miranda</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__GUILERME_GUERREIRO.mp3" class="newWindow">
+					<p>Guilherme Guerreiro Neto</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__GUILERME_GUERREIRO_PAI.mp3" class="newWindow">
+					<p>Guilherme Guerreiro Filho</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__JOAO_JADSON.mp3" class="newWindow">
+					<p>João Jadson</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__JOAO_NILO.mp3" class="newWindow">
+					<p>João Nilo</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__LIVIA_BARBOSA.mp3" class="newWindow">
+					<p>Lívia Barbosa</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__LIVIA_COLARES.mp3" class="newWindow">
+					<p>Livia Colares</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__LUCIANA_MIRANDA.mp3" class="newWindow">
+					<p>Luciana Miranda</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__LUCIO_FLAVIO_PINTO.mp3" class="newWindow">
+					<p>Lúcio Flávio Pinto</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__LUIZ_FERNANDO_MACHADO.mp3" class="newWindow">
+					<p>Luiz Fernando Machado</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__MARCOS_PASSOS.mp3" class="newWindow">
+					<p>Marcos Passos</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__NATHAN.mp3" class="newWindow">
+					<p>Nathan Nguangu</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__OTACILIO_AMARAL.mp3" class="newWindow">
+					<p>Otacílio Amaral</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__ROSALY_BRITO.mp3" class="newWindow">
+					<p>Rosaly Brito</p>
+				</a>
+
+				<a href="http://radio.ufpa.br/media/audio/facom40anos/DEPOIMENTOS_FACOM__ROSANE_STEINBRENNER.mp3" class="newWindow">
+					<p>Rosane Steinbrenner</p>
+				</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+
+	<section id="materias" class="full-screen img-2">
+
+        <div class="full-screen-container invert color-5">
+
+			<h2>Matérias</h2>
+
+			<a href="http://radio.ufpa.br/novaradio/index.php/news/mem%C3%B3rias,-encontros-e-reflex%C3%B5es-nos-40-anos-da-facom" target="_blank" class="post">
+				<figure style="background-image: url('');"></figure>
+				<span>Rádio Web UFPA</span>
+
+				<h3>Memórias, encontros e reflexões nos 40 anos da Facom</h3>
+
+				<p>A Universidade é um ambiente que traz aprendizados que vão além dos muros do campus. Para muitos, o processo de formação profissional vem acompanhado de um intenso amadurecimento pessoal, por isso propor lições que façam os alunos pensar de forma crítica, política e humanizadora é também uma das missões desse ambiente...</p>
+			</a>
+
+			<a href="http://radio.ufpa.br/novaradio/index.php/news/r%C3%A1dio-web-comemora-40-anos-do-curso-de-comunica%C3%A7%C3%A3o-com-programa%C3%A7%C3%A3o-especial" target="_blank" class="post">
+				<figure style="background-image: url('');"></figure>
+				<span>Rádio Web UFPA</span>
+
+				<h3>Rádio Web comemora 40 anos do curso de comunicação com programação especial</h3>
+
+				<p>Durante o mês de fevereiro, a Rádio Web da UFPA preparou uma programação especial em homenagem aos 40 anos da Faculdade de Comunicação (Facom) da UFPA. A programação especial entrou no ar desde o dia primeiro deste mês...</p>
+			</a>
+
+        </div>
+
+	</section>
+
+	<footer class="color-1">
+
+		<?php include '../_includes/footer.php'; ?>
+		
+	</footer>
+
+<!-- Scripts -->
+<script src="http://radio.ufpa.br/_scripts/functions.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/menu.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/header.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/scroll.js" type="text/javascript"></script>
+
+</body>
+</html>
